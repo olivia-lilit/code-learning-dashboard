3c608f5a8d4e5dc505fba4ff082acb5032da4c19
Olivia Rudd <oliviadavidsonrudd@gmail.com>
2022-02-09 20:34:41 -0500

    Make a useful(?) mess

--- playgound/play.js
+++ playgound/play.js
@@ -1,18 +1,34 @@
-let newProgressTracker= document.createElement("progress");
+// let newProgressTracker= document.createElement("progress");
 
-function addProgressTracker() {
-    document.body.append(newProgressTracker);
-    console.log("added!")
+// function addProgressTracker() {
+//     document.body.append(newProgressTracker);
+//     console.log("added!")
+// }
+
+// let buttons = document.querySelectorAll("button");
+// console.log(buttons.length)
+// for (button of buttons) {
+// console.log(button.innerHTML);
+// button.addEventListener("click", addProgressTracker);
+
+// }
+
+let trackers = [];
+let form = document.querySelector("form");
+
+function newTracker() {
+    trackers.push(
+        {id: trackers.length+1, 
+        resourceName: (form.elements[0].textContent)
+        }
+    );
+    console.log(trackers);
+    console.log((form.elements[0].value))
 }
 
 let buttons = document.querySelectorAll("button");
 console.log(buttons.length)
 for (button of buttons) {
-console.log(button.innerHTML);
-button.addEventListener("click", addProgressTracker);
-
+button.addEventListener("click", newTracker);
 }
-
-
-
 
--- src/routes/addProgressBar.svelte
+++ src/routes/addProgressBar.svelte
@@ -1,9 +1,12 @@
 <script>
 function addProgressTracker(){
     let newProgressTracker= document.createElement("progress");
-    document.body.append(newProgressTracker);
+    newProgressTracker.style.display = "block"
+    let button = document.querySelector("button");
+    button.after(newProgressTracker);
     console.log("added!")
 }
+
 </script>
 
 <button on:click={addProgressTracker}>Add Tracker</button>
@@ -13,43 +16,5 @@ function addProgressTracker(){
         font-family: monospace;
     }
 
-    progress{
-    -webkit-appearance: none;
-       -moz-appearance: none;
-            appearance: none;
-            border: none;
-    background-image:
-      -moz-linear-gradient(
-        top, 
-        rgba(255, 255, 255, 0.25), 
-        rgba(0, 0, 0, 0.25)
-      ),
-      -moz-linear-gradient(
-        left, 
-        rgb(76, 0, 255), 
-        rgb(216, 0, 144)
-      );
-  
-    border-radius: 2px; 
-    background-size: 35px 20px, 100% 100%, 100% 100%; 
-  }
 
-  
-
-  progress[value]::-moz-progress-bar { 
-    background-image:
-      -moz-linear-gradient(
-        top, 
-        rgba(255, 255, 255, 0.25), 
-        rgba(0, 0, 0, 0.25)
-      ),
-      -moz-linear-gradient(
-        left, 
-        rgb(76, 0, 255), 
-        rgb(216, 0, 144)
-      );
-  
-    border-radius: 2px; 
-    background-size: 35px 20px, 100% 100%, 100% 100%; 
-  }
 </style>
