6f22d59912bf379d4c017f191d4b461d0583212f
Olivia Rudd <oliviadavidsonrudd@gmail.com>
2022-02-13 16:15:41 -0500

    Spacing changes

--- src/components/trackers.svelte
+++ src/components/trackers.svelte
@@ -4,10 +4,14 @@ export let trackers;
 let newTrackerName; 
 let newTrackerTotalModules;
 let newTrackerCompletedModules;
-let newTrackerId; 
+let newTrackerId= "";
+$: console.log(`length is ${trackers.length}`);
+$: console.log(`new id is ${newTrackerId}`);
+$: console.log(trackers);
+
 $: {
-    if (trackers.length ===0){
-        newTrackerId = 2;
+    if (trackers.length === 0){
+        newTrackerId = 1;
     }
     else {
         newTrackerId = Math.max(...trackers.map(t => t.id)) + 1;
@@ -16,6 +20,11 @@ $: {
 
 function addTracker() {
     trackers = [...trackers, {id:newTrackerId, name:newTrackerName, totalModules:newTrackerTotalModules, completedModules:newTrackerCompletedModules}]
+    newTrackerName = "";
+    newTrackerTotalModules="";
+    newTrackerCompletedModules="";
+    newTrackerId="";
+
 }
 
 </script>
@@ -39,7 +48,7 @@ function addTracker() {
 
 
 <ul>
-    {#each trackers as tracker (trackers.id)}
+    {#each trackers as tracker (tracker.id)}
     <li class="tracker"> 
         <label for="{tracker.id}">{tracker.name}</label>
         <progress id="{tracker.id}" max="{tracker.totalModules}" value="{tracker.completedModules}"> </progress>
