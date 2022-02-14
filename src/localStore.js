8fd9a8f7c5402bf8837d7e7b730ba9b300af74c3
Olivia Rudd <oliviadavidsonrudd@gmail.com>
2022-02-13 22:57:11 -0500

    Attempting

--- src/localStore.js
+++ src/localStore.js
@@ -1,20 +1,6 @@
-import { browser } from "$app/env";
-// import { localStorageStore as base } from"/Users/oliviarudd/Repos/learning-dash/node_modules/@babichjacob/svelte-localstorage/base.js"
 
-import { writable } from 'svelte/store';
-
-
-// /**
-//  * @template Item
-//  * @param {string} key What key in localStorage to synchronize with
-//  * @param {Item} initial The initial value of the writable store
-//  * @returns {import("svelte/store").Writable<Item>} A writable store that synchronizes to localStorage
-//  */
-//  export const localStorageStore = (key, initial) => base(key, initial, browser);
-
-// export const localStore = (key, initial) => {                 
+import { writable } from 'svelte/store';          
     // receives the key of the local storage and an initial value
-  if (browser) {
   const toString = (value) => JSON.stringify(value, null, 2);  
   // helper function
   // const toObj = JSON.parse();
