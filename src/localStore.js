e635a00a2f9fa2316dfd343a6f59465cf34976d4
Olivia Rudd <oliviadavidsonrudd@gmail.com>
2022-02-24 22:56:55 -0500

    Format and standardize naming convention

--- src/localStore.js
+++ src/localStore.js
@@ -1,31 +1,30 @@
+import { writable } from 'svelte/store';
+// receives the key of the local storage and an initial value
+const toString = (value) => JSON.stringify(value, null, 2);
+// helper function
+// const toObj = JSON.parse();
 
-import { writable } from 'svelte/store';          
-    // receives the key of the local storage and an initial value
-  const toString = (value) => JSON.stringify(value, null, 2);  
-  // helper function
-  // const toObj = JSON.parse();
+const toObj = (value) => JSON.parse(value);
+// helper function
 
-  const toObj = (value) =>JSON.parse(value);
-  // helper function
+if (localStorage.getItem(key) === null) {
+	// item not present in local storage
+	localStorage.setItem(key, toString(initial));
+	// initialize local storage with initial value
+}
 
-  if (localStorage.getItem(key) === null) {                   
-      // item not present in local storage
-    localStorage.setItem(key, toString(initial))              
-    // initialize local storage with initial value
-  }
+const saved = toObj(localStorage.getItem(key));
+// convert to object
 
-  const saved = toObj(localStorage.getItem(key))             
-   // convert to object
+const { subscribe, set, update } = writable(saved);
+// create the underlying writable store
 
-  const { subscribe, set, update } = writable(saved)          
-  // create the underlying writable store
-
-  return {
-    subscribe,
-    set: (value) => {
-      localStorage.setItem(key, toString(value))              
-      // save also to local storage as a string
-      return set(value)
-    },
-    update
-  }
+return {
+	subscribe,
+	set: (value) => {
+		localStorage.setItem(key, toString(value));
+		// save also to local storage as a string
+		return set(value);
+	},
+	update
+};
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,12 @@
 <script>
-    import "../globalStyles.css"
-    import TrackersList from "../components/trackers.svelte"
-    import { trackers } from "../stores"
+	import '../globalStyles.css';
+	import TrackersList from '../components/trackers.svelte';
+	import { trackers } from '../stores';
 
-    import HelloName from "../components/helloName.svelte"
-    import { userName } from "../stores"
-
-
+	import HelloName from '../components/helloName.svelte';
+	import { userName } from '../stores';
 </script>
 
-<HelloName bind:userName={$userName}/>
+<HelloName bind:userName={$userName} />
 
-
-<TrackersList bind:trackers={$trackers} /> 
+<TrackersList bind:trackers={$trackers} />
