5c2b05f3321eac2dc37168c97851e4d2cb3458a8
Olivia Rudd <oliviadavidsonrudd@gmail.com>
2022-02-24 22:10:37 -0500

    Update default greeting

--- src/stores.js
+++ src/stores.js
@@ -8,6 +8,6 @@ export const trackers = localStorageStore("tracker-data", initialTrackers);
 
 // stands to reason you can just repeat the above a few times for things like name, focus, bookmarks
 
-const initialUserName = "bees..."
+const initialUserName = "{your name}"
 
 export const userName = localStorageStore("user-name", initialUserName)
